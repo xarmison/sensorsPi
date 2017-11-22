e9fe6148a26762e11885dbf34bb712dd2872782d
vanluwin <richardson-santiago@hotmail.com>
2017-11-22 10:14:24 -0300

    interface para vizualização dos graficos

--- pi/interface/public/js/interface.js
+++ pi/interface/public/js/interface.js
@@ -1,20 +1,64 @@
 // Funcção para controlar o lamp
-$(document).ready(function() {
+$(document).ready(() => {
 
     // Refresh sensor data
     refreshSensors();
-    setInterval(refreshSensors, 5000);
+    setInterval(refreshSensors, 1000);
+    setInterval(removeAllData, 60000);
 
 });
-    
+
+//Adiciona novos dados ao gráfico com o horario atual como rotulo
+function addData(chart, data) {
+    let time = new Date();
+    chart.data.labels.push(time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds());
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+//Limpa os dados de um gráfico
+function removeData(chart) {
+    chart.data.labels = [];
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = [];
+    });
+    chart.update();
+}
+//limpa os dados de todos os gráfico
+function removeAllData(){
+    removeData(tempGraph);
+    removeData(umiGraph);
+    removeData(ldrGraph);
+}
+
+
+
 function refreshSensors() {
-
-    $.get('/sensor_module/temperature', function(json_data) {
-    $("#temperatura").text(json_data.temperature + ' *C');
-
-    $.get('/sensor_module/humidity', function(json_data) {
+    /*
+    $.get('http://192.168.2.6/sensors/temperature', (json_data) => {
+        $("#temperatura").text(json_data.temperature + ' °C');
+        addData(tempGraph, json_data.temperature);
+    });
+            
+    $.get('http://192.168.2.6/sensors/humidity', function (json_data) {
         $("#umidade_ar").text(json_data.humidity + ' %');
-    });
+        addData(umiGraph, json_data.humidity);
     });
 
+    $.get('http://192.168.2.6/sensors/ldr', function (json_data) {
+        $("#ldr").text(json_data.ldr);
+        addData(ldrGraph, json_data.ldr);
+    });
+
+    $.get('http://192.168.2.6/sensors/distance', function (json_data) {
+        $("#distance").text(json_data.distance + ' cm');
+    });
+    */
+    $.get('http://192.168.2.6/rfid', (json_data) => {
+        $("#rfid_log").text("ID do cartão: " + json_data.uid);
+        $("#rfid_log").text("\n" + json_data.acess);
+    });
+    
 }
